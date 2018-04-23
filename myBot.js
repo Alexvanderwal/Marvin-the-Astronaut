e68c7e36e57e369a4c44997c8bbb7995a406ff1f
Mika <mikavandenbrink@outlook.com>
2018-04-23 18:23:47 +0200

    New base for the bot

--- myBot.js
+++ myBot.js
@@ -17,7 +17,6 @@ client.on("message", (message) => {
   if(message.author.bot) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  let emote = config.prefix;
   // Exit and stop if it's not there
   if (!message.content.startsWith(config.prefix)) return;
 
@@ -30,21 +29,30 @@ client.on("message", (message) => {
     let age = args[0]; // Remember arrays are 0-based!.
     let sex = args[1];
     let location = args[2];
-    message.reply(`Hallo ${message.author.username}, Ik zie dat je ${age} jaar oud en een ${sex} bent uit ${location}. Wil je me klapjes geven?`);
+    message.channel.send(`Hallo ${message.author.username}, Ik zie dat je ${age} jaar oud en een ${sex} bent uit ${location}. Wil je me klapjes geven?`);
   }
 
   if (command === "avatar"){
     message.reply(message.author.avatarURL);
   }
 
-  if (command === "wat is het beste nummer ooit"){
-    message.reply('Ey swa, gozer. Dat lijkt me nogal wiedes, ea. Is Toto Africa natuurlijk, of nie dan.', emote)
-    message.reply('https://open.spotify.com/track/2374M0fQpWi3dLnB54qaLX?si=UxTPgKJrR9i8kELKmUaaog');
+  if (command === "bestenummer"){
+    message.channel.send('Ey swa, gozer. Dat lijkt me nogal wiedes, ea. Is Toto Africa natuurlijk, of nie dan.')
+    message.channel.send('https://open.spotify.com/track/2374M0fQpWi3dLnB54qaLX?si=UxTPgKJrR9i8kELKmUaaog');
   }
 
-  if (command === "waar zit je?"){
-    message.channel.send("Ikke zit gewoon effe lekker te bakken op de maan, ja. Kusjes en een zelfje", emote,
-    {files: ["https://i.pinimg.com/originals/26/cf/26/26cf26b1e1e98ae0eb49bf5e58736cb2.jpg"]});
+  if (command === "stuurselfie"){
+    message.channel.send("Ikke zit gewoon effe lekker te bakken op de maan, ja. Kusjes en een zelfje",
+    {"files": ["https://i.pinimg.com/originals/26/cf/26/26cf26b1e1e98ae0eb49bf5e58736cb2.jpg"]});
+  }
+
+  if (command === "bak"){
+    message.channel.send(`haha, onze beste makker ${message.author} gaat een rookje doen.`);
+  }
+  //nutteloos lijstje van hoe high ik ben
+  if (command === "0"){
+    message.channel.send(`Het ziet er naar uit dat ${message.author} nog op planeet Aarde is.`),
+    message.channel.send(`Kan iemand wat wiet voor hem regelen?! <:wooooOOOOOOOOOOWWWWWW:432266541778731018>`);
   }
 
   // Admin commands
